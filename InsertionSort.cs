e60da326d1ee42a030be49a69ab051142f50e436
Praful <prafulrakhade02@gmail.com>
2022-05-23 14:47:36 +0530

    [praful] Added : Insertion sort program file

---
+++ InsertionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    internal class InsertionSort
+    {
+        public static void sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = temp;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Insertion Sorted array is: ");
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n------------WELCOME TO THE INSERTION SORTING PROGRAM----------------\n\n");
+            Console.Write("Enter the length of array : ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+            Console.WriteLine("Enter the element of array :");
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            InsertionSort.sort(arr);
+        }
+    }
+}
