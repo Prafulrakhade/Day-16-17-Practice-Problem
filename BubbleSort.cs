40171394d6e9567a5d53f3d1b1bbda0828bd4458
Praful <prafulrakhade02@gmail.com>
2022-05-23 13:19:05 +0530

    [praful] Added : Bubble sort program file

---
+++ BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortProgram
+{
+    internal class BubbleSort
+    {
+        public static void BubbleSorting(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])   //its for descending order  And if(arr[j] > arr[j + 1]) ascending order
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            //Display unit
+            Console.WriteLine("Bubble Sorted array is: ");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BubbleSortProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n-----------------Welcome to BubbleSort Program------------\n\n");
+            Console.Write("Please enter the length of the Array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Please enter the array elements : ");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            BubbleSort.BubbleSorting(arr);
+        }
+    }
+}
